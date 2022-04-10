8d882da51efbf6113d3d8a73f66aba5e4f4929fd
Your Name <you@example.com>
2022-04-10 00:02:33 -0400

    newproject

---
+++ Apple.java
@@ -0,0 +1,22 @@
+package day4;
+
+public class FruitsFarmColors {
+	public static void main(String[] args) {
+		String apple = "Green";
+		String banana = "Yellow";
+		String orange = "pink";
+		String kiwi = "blue";
+		String pear = "Rad";
+		
+		System.out.println(apple); // green
+		System.out.println(banana); // yellow
+		System.out.println(orange); // pink
+		System.out.println(kiwi); // blue
+		System.out.println(pear); // red
+		
+		String allFruits = apple + "," + banana + "," + orange + "," + kiwi + "," + pear;
+		System.out.println("Fruits: " + allFruits);
+		
+	}
+
+}
